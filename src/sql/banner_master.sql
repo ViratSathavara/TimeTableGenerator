eeaf05a5256f326f517f5550472ba4ec2dce4f21
Jay Suthar <jaysuthar67@gmail.com>
2023-04-26 23:49:45 +0530

    sql

--- src/sql/banner_master.sql
+++ src/sql/banner_master.sql
@@ -14,3 +14,18 @@ CREATE TABLE `banner_master` (
     KEY `ix_ispublished` (`ispublished`) USING BTREE,
 	PRIMARY KEY (`id`)
 );
+
+
+
+-- Do Not Execute on Prod - Only Testing Queries
+INSERT INTO banner_master SET title = "test title 1",description = "test description 1 - TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST ",createdby =1,modifedby =  1, ispublished = 1;
+INSERT INTO banner_master SET title = "test title 2",description = "test description 2 - TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST ",createdby =1,modifedby =  1, ispublished = 1;
+INSERT INTO banner_master SET title = "test title 3",description = "test description 3 - TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST ",createdby =1,modifedby =  1, ispublished = 1;
+INSERT INTO banner_master SET title = "test title 4",description = "test description 4 - TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST ",createdby =1,modifedby =  1, ispublished = 1;
+INSERT INTO banner_master SET title = "test title 4",description = "test description 4 - TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST ",createdby =1,modifedby =  1, ispublished = 1;
+INSERT INTO banner_master SET title = "test title 5",description = "test description 5 - TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST ",createdby =1,modifedby =  1, ispublished = 1;
+INSERT INTO banner_master SET title = "test title 6",description = "test description 6 - TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST ",createdby =1,modifedby =  1, ispublished = 1;
+INSERT INTO banner_master SET title = "test title 7",description = "test description 7 - TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST ",createdby =1,modifedby =  1, ispublished = 1;
+INSERT INTO banner_master SET title = "test title 8",description = "test description 8 - TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST ",createdby =1,modifedby =  1, ispublished = 1;
+INSERT INTO banner_master SET title = "test title 9",description = "test description 9 - TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST ",createdby =1,modifedby =  1, ispublished = 1;
+INSERT INTO banner_master SET title = "test title 10",description = "test description 10 - TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST ",createdby =1,modifedby =  1, ispublished = 1;
