d283461a7d0b68405a61ddfed2d94c1f9b7b3b6b
Jay Suthar <jaysuthar67@gmail.com>
2023-04-26 23:41:43 +0530

    added sql file

---
+++ src/sql/banner_master.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `banner_master` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR(255),
+	`description` TEXT,
+	`ispublished` INT DEFAULT '0',
+	`createdby` INT,
+	`createdon` DATETIME DEFAULT CURRENT_TIMESTAMP,
+	`modifedby` INT,
+	`modifiedon` DATETIME DEFAULT CURRENT_TIMESTAMP,
+	`delflag` INT DEFAULT '0',
+	KEY `ix_id` (`title`) USING BTREE,
+    KEY `ix_title` (`title`) USING BTREE,
+    KEY `ix_delflag` (`delflag`) USING BTREE,
+    KEY `ix_createdby` (`createdby`) USING BTREE,
+    KEY `ix_ispublished` (`ispublished`) USING BTREE,
+	PRIMARY KEY (`id`)
+);
